f71e44931960295dd0bb7d3b44088146901353b7
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-22 15:45:07 +0530

    creating a register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <form >
+        <h3 className="text-center mb-3">Create an account</h3>
+        <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+                Name
+            </label>
+        </div>
+    </form>
+  );
+};
 
-export default Register
+export default Register;
