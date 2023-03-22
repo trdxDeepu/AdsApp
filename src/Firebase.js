992cdf58e5953f7bba8e5ee4f5059dbbd5da2420
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-22 15:04:42 +0530

    added env and firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPI3WP2gaH-PToXA-FWrIsEa6AxfpNGy0",
-  authDomain: "adsnetwork-a0699.firebaseapp.com",
-  projectId: "adsnetwork-a0699",
-  storageBucket: "adsnetwork-a0699.appspot.com",
-  messagingSenderId: "450731233006",
-  appId: "1:450731233006:web:a325c4aa52024427e26dff"
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  databaseUrl: process.env.REACT_APP_DATABASE_URL,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
