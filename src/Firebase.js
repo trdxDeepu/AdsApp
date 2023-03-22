15bcff623c56091432c9409c6aa76fc537cd873e
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-22 14:49:31 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPI3WP2gaH-PToXA-FWrIsEa6AxfpNGy0",
+  authDomain: "adsnetwork-a0699.firebaseapp.com",
+  projectId: "adsnetwork-a0699",
+  storageBucket: "adsnetwork-a0699.appspot.com",
+  messagingSenderId: "450731233006",
+  appId: "1:450731233006:web:a325c4aa52024427e26dff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
